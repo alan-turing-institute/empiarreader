7dbcd229f1fce9a78081e95f4a958f01575479e6
Oliver Strickson <ostrickson@turing.ac.uk>
2022-03-07 15:11:20 +0000

    Add tests for MrcSource

---
+++ tests/test_load_mrc.py
@@ -0,0 +1,25 @@
+import intake
+import numpy as np
+import mrcfile
+
+
+def test_load_mrc():
+    data = np.arange(9, dtype="float32").reshape((3, 3))
+
+    with mrcfile.new("tmp.mrc", overwrite=True) as example_mrc:
+        example_mrc.set_data(data)
+
+    ds = intake.open_mrc("tmp.mrc")
+
+    print(ds)
+
+    data_read = ds.read()
+
+    assert data == data_read
+
+
+def test_load_remote_mrc():
+    ds = intake.open_mrc(
+        "https://github.com/ccpem/mrcfile/blob/master/tests/test_data/epu2.9_example.mrc?raw=true"
+    )
+    data = ds.read()
