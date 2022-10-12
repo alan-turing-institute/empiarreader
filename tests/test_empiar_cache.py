1c29fceddb32aa9645b1cc49d120469c1fa7e727
Joel Greer <jg13994@my.bristol.ac.uk>
2022-10-12 14:19:20 +0000

    updated poetry and pyproject with new script investigating grabbing data from a dataset in addition to grabbing metadata. Which is broken/incorrectly written

--- tests/test_empiar_cache.py
+++ tests/test_empiar_cache.py
@@ -1,3 +1,4 @@
+from unittest.mock import patch
 from intake.config import conf
 
 from intake_cryoem.empiar import EmpiarCatalog, EmpiarSource
@@ -23,6 +24,10 @@ def test_empiar_cache():
     print(list(cat))
     print('\n\n')
 
+    ##############################
+    # now looking at the EmpiarCatalog.fetch_entry_data
+    print('EmpiarCatalog.fetch_entry_data:{}'.format(cat.fetch_entry_data(10340)))
+
     # I think cat(alogue) is a list of imagesets
     # So EmpiarCatalogue() starts by grabbing the name of the imagesets by reading them
     # from the online path (EmpiarCatalogue.fetch_entrydata()). 
@@ -43,18 +48,15 @@ def test_empiar_cache():
     
     print('\n\n')
 
+    # partition = cat.read_partition(0)
+    # print('Partition: {}'.format(partition))
+
+
     # I don't think you re actually using the cache yet as you seem to only have grabbed metadata
     # Try grabbing an actual data file and see what happens... do you use cache?
     ds = cat["Unaligned movies for Case 1"]
     ds.read_partition(0)
     
-
-
-
-    """
-    for i,key in enumerate(cat.keys):
-        print('{}: {}'.format(i, key))
-    """
     return
 
     assert "Unaligned movies for Case 1" in cat.keys()
@@ -67,5 +69,40 @@ def test_empiar_cache():
 
     # Note: ds.read() is expensive
 
+def test_empiar_download():
+    conf['cache_download_progress'] = True
+    conf['cache_dir'] = os.path.join(os.getcwd(), 'test_cache_dir')
+    # Grab the metadata from MEPIAR entry 10340
+    cat = EmpiarCatalog(10340)
+    print('Cat start')
+    print(cat)
+    print(list(cat))
+    print('\n\n')
+    # Grab the first entry (an EmpiarSource)
+    ds = cat[list(cat)[0]]
+    print('ds start')
+    print('ds type {}'.format(type(ds)))
+    print(ds)
+    #print(list(ds))
+    assert isinstance(ds, EmpiarSource)
+    assert ds.directory == "data/Movies/Case1"
+    print('Imageset EMPIAR index: {}'.format(ds.empiar_index))
+    print('Imageset directory: {}'.format(ds.directory))
+    print('Imageset metadata: {}'.format(ds.imageset_metadata))
+    print('Imageset driver: {}'.format(ds._driver))
+    print('Imageset image urls: {}'.format(ds._image_urls))
+    print('Imageset datasource: {}'.format(ds._datasource))
+    read_data = ds.read()
+    print(type(read_data))
+    print('Read data: {}'.format(read_data))
+
+
+    
+    ds.read_partition(0)
+
+
+
+
 if __name__ == '__main__':
-    test_empiar_cache()
+    # test_empiar_cache()
+    test_empiar_download()
